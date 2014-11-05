7e2a8eeec41304b240ebaf3de4635e2c007b9a7d
kpurdon <kylepurdon@gmail.com>
2014-11-04 21:41:08 -0700

    add click acceptance tests

--- pymr/tests/test_register.py
+++ pymr/tests/test_register.py
@@ -1,12 +1,12 @@
+import os
 import unittest
+
+from click.testing import CliRunner
 
 from pymr import register
 
 
 class TestRegiser(unittest.TestCase):
-
-    def setUp(self):
-        self.foo = 'bar'
 
     def test_default_tag_is_set(self):
         expected = 'default'
@@ -25,3 +25,46 @@ class TestRegiser(unittest.TestCase):
         actual = register.parse_tag('foo')
 
         self.assertEqual(expected, actual)
+
+    def test_register_command_creates_file(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(register.register)
+            assert not result.exception
+            assert os.path.exists('.pymr')
+
+    def test_register_command_creates_default_tag(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(register.register)
+            assert not result.exception
+
+            expected = '[tags]\ntags = default\n\n'
+            with open('.pymr', 'r') as f:
+                actual = f.read()
+
+            self.assertEqual(expected, actual)
+
+    def test_register_command_creates_single_tag(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(register.register, args=['-ttest'])
+            assert not result.exception
+
+            expected = '[tags]\ntags = test\n\n'
+            with open('.pymr', 'r') as f:
+                actual = f.read()
+
+            self.assertEqual(expected, actual)
+
+    def test_register_command_creates_multiple_tags(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(register.register, args=['-ttest', '-ttest2'])
+            assert not result.exception
+
+            expected = '[tags]\ntags = test,test2\n\n'
+            with open('.pymr', 'r') as f:
+                actual = f.read()
+
+            self.assertEqual(expected, actual)
--- pymr/tests/test_run.py
+++ pymr/tests/test_run.py
@@ -1,12 +1,12 @@
+import os
 import unittest
+
+from click.testing import CliRunner
 
 from pymr import run
 
 
 class TestRun(unittest.TestCase):
-
-    def setUp(self):
-        self.foo = 'bar'
 
     def test_default_tag_is_set(self):
         expected = 'default'
@@ -31,3 +31,39 @@ class TestRun(unittest.TestCase):
         actual = run.unpack_tags('foo,bar')
 
         self.assertEqual(expected, actual)
+
+    def test_run_command_finds_default(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open('.pymr', 'w') as f:
+                f.write('[tags]\ntags = default\n\n')
+
+            expected = 'calling echo in ./.pymr\n'
+            result = runner.invoke(run.run, args=['echo'])
+
+            assert not result.exception
+            assert result.output == expected
+
+    def test_run_command_finds_tags(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open('.pymr', 'w') as f:
+                f.write('[tags]\ntags = test\n\n')
+
+            expected = 'calling echo in ./.pymr\n'
+            result = runner.invoke(run.run, args=['-ttest', 'echo'])
+
+            assert not result.exception
+            assert result.output == expected
+
+    def test_run_command_finds_tags_when_multiple_tags_exist(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open('.pymr', 'w') as f:
+                f.write('[tags]\ntags = test,test2\n\n')
+
+            expected = 'calling echo in ./.pymr\n'
+            result = runner.invoke(run.run, args=['-ttest2', 'echo'])
+
+            assert not result.exception
+            assert result.output == expected
